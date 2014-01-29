78f9b7257ef693d91e7832d71512af862655f6c2
Khairul Anwar (kulll) <anwarbaik88@gmail.com>
2014-01-29 13:18:25 +0800

    add upgrade profile for consultant (re #1030)

--- ploneun/consultant/upgrades/handlers.py
+++ ploneun/consultant/upgrades/handlers.py
@@ -1,7 +1,24 @@
 from collective.grok import gs
 from Products.CMFCore.utils import getToolByName
+from zope.component.hooks import getSite
 
 # -*- extra stuff goes here -*- 
+
+
+@gs.upgradestep(title=u'Upgrade ploneun.consultant to 3',
+                description=u'Upgrade ploneun.consultant to 3',
+                source='2', destination='3',
+                sortkey=1, profile='ploneun.consultant:default')
+def to3(context):
+    setup = getToolByName(context, 'portal_setup')
+    site = getSite().id
+    if site in 'eval':
+        catalog = getToolByName(context, 'portal_catalog')
+        for brain in catalog(portal_type="ploneun.consultant.consultant"):
+            obj = brain.getObject()
+            del obj.functions
+    else:
+        setup.runAllImportStepsFromProfile('profile-ploneun.consultant.upgrades:to3')
 
 
 @gs.upgradestep(title=u'Upgrade ploneun.consultant to 2',
--- ploneun/consultant/upgrades/profiles.py
+++ ploneun/consultant/upgrades/profiles.py
@@ -2,6 +2,11 @@ from collective.grok import gs
 
 # -*- extra stuff goes here -*- 
 
+gs.profile(name=u'to3',
+           title=u'Upgrade ploneun.consultant to 3',
+           description='',
+           directory='profiles/to3')
+
 gs.profile(name=u'to2',
            title=u'Upgrade ploneun.consultant to 2',
            description='',
